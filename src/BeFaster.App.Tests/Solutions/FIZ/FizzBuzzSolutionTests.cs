5ad20347633510837dd249aca8cffec02b29fc10
sbx_user1051 <sbx_user1051@169.254.150.125>
2020-11-23 17:50:19 +0000

    Mon Nov 23 17:50:19 UTC 2020

--- src/BeFaster.App.Tests/Solutions/FIZ/FizzBuzzSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/FIZ/FizzBuzzSolutionTests.cs
@@ -14,7 +14,7 @@ namespace BeFaster.App.Tests.Solutions.FIZ
         [Test]
         [TestCase(1, ExpectedResult = "1")]
         [TestCase(2, ExpectedResult = "2")]
-        [TestCase(3, ExpectedResult = "fizz")]
+        [TestCase(3, ExpectedResult = "fizz deluxe")]
         [TestCase(4, ExpectedResult = "4")]
         [TestCase(5, ExpectedResult = "buzz")]
         [TestCase(6, ExpectedResult = "fizz")]
@@ -22,39 +22,44 @@ namespace BeFaster.App.Tests.Solutions.FIZ
         [TestCase(8, ExpectedResult = "8")]
         [TestCase(9, ExpectedResult = "fizz")]
         [TestCase(10, ExpectedResult = "buzz")]
-        [TestCase(11, ExpectedResult = "fake deluxe")]
+        [TestCase(11, ExpectedResult = "11")]
         [TestCase(12, ExpectedResult = "fizz")]
         [TestCase(13, ExpectedResult = "fizz")]
         [TestCase(14, ExpectedResult = "14")]
         [TestCase(15, ExpectedResult = "fizz buzz")]
-        [TestCase(22, ExpectedResult = "deluxe")]
-        [TestCase(30, ExpectedResult = "fizz buzz")]
+        [TestCase(22, ExpectedResult = "22")]
+        [TestCase(30, ExpectedResult = "fizz buzz deluxe")]
         [TestCase(31, ExpectedResult = "fizz")]
         [TestCase(32, ExpectedResult = "fizz")]
         [TestCase(33, ExpectedResult = "fizz fake deluxe")]
         [TestCase(34, ExpectedResult = "fizz")]
         [TestCase(35, ExpectedResult = "fizz buzz")]
+        [TestCase(36, ExpectedResult = "fizz deluxe")]
         [TestCase(40, ExpectedResult = "buzz")]
         [TestCase(41, ExpectedResult = "41")]
         [TestCase(42, ExpectedResult = "fizz")]
         [TestCase(43, ExpectedResult = "fizz")]
-        [TestCase(44, ExpectedResult = "deluxe")]
+        [TestCase(44, ExpectedResult = "44")]
         [TestCase(45, ExpectedResult = "fizz buzz")]
         [TestCase(50, ExpectedResult = "buzz")]
         [TestCase(51, ExpectedResult = "fizz buzz")]
         [TestCase(52, ExpectedResult = "buzz")]
         [TestCase(53, ExpectedResult = "fizz buzz")]
         [TestCase(54, ExpectedResult = "fizz buzz")]
-        [TestCase(55, ExpectedResult = "buzz fake deluxe")]
-        [TestCase(555, ExpectedResult = "fizz buzz fake deluxe")]
+        [TestCase(55, ExpectedResult = "buzz")]
+        [TestCase(63, ExpectedResult = "fizz deluxe")]
+        [TestCase(555, ExpectedResult = "fizz buzz")]
         [TestCase(566, ExpectedResult = "buzz")]
-        [TestCase(666, ExpectedResult = "fizz deluxe")]
+        [TestCase(666, ExpectedResult = "fizz")]
+        [TestCase(135, ExpectedResult = "fizz buzz fake deluxe")]
+        [TestCase(2325, ExpectedResult = "fizz buzz deluxe")]
         public string TestFizzBuzz(int number)
         {
             return FizzBuzzSolution.FizzBuzz(number);
         }
     }
 }
+
 
 
 
