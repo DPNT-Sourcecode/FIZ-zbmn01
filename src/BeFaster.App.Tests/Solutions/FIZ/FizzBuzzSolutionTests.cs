38af3d1057dcc5ad664ef322b05ff430bc2852f6
sbx_user1051 <sbx_user1051@169.254.150.125>
2020-11-23 17:24:42 +0000

    Mon Nov 23 17:24:42 UTC 2020

---
+++ src/BeFaster.App.Tests/Solutions/FIZ/FizzBuzzSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Tests.Solutions.FIZ
+{
+    [TestFixture]
+    class FizzBuzzSolutionTests
+    {
+        [Test]
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(2, ExpectedResult = "2")]
+        [TestCase(3, ExpectedResult = "fizz")]
+        public string TestFizzBuzz(int number)
+        {
+            return number.ToString();
+        }
+    }
+}
